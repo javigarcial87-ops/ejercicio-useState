238a96e414fb7107012ee19fc91a77d5024d0bfb
javier garcia alvarez <javigarcial87@gmail.com>
2026-02-23 19:08:52 +0100

    app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import './App.css'
+import {useState} from "react"
 
 function App() {
- //Aquí tu código
+ 
+    const [name, setName] = useState("Sofía")
+
+    return (
+        <div>
+            <h2>Nombre del profesor:{name}</h2>
+
+            <ul>
+                <li onClick={()=> setName("Data")}>Data</li>
+                <li onClick={()=> setName("Reyes")}>Reyes</li>
+                <li onClick={()=> setName("Yolanda")}>Yolanda</li>
+            </ul>
+
+        </div>
+    )
+
 }
 
 export default App
