59fe899527b67b9c8d79e0284c5c88cd1fd56075
javier garcia alvarez <javigarcial87@gmail.com>
2026-02-23 19:35:03 +0100

    bonus

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,30 @@ import {useState} from "react"
 function App() {
  
     const [name, setName] = useState("Sofía")
+    const {newName,setNewName} = useState("")
+
+    const changeName = (e)=> {
+        e.preventDefault()
+
+        if (newName.trim()!=="") {
+            setName(newName)
+            setNewName("")
+        }
+    }
 
     return (
         <div>
             <h2>Nombre del profesor:{name}</h2>
+
+            <form onSubmit={changeName} >
+                <input 
+                type="text"
+                value ={newName}
+                onChange={(e) => setName(e.target.value)}
+                placeholder='añade un nombre' />
+
+                <button type="submit">Añadir</button>
+            </form>
 
             <ul>
                 <li onClick={()=> setName("Data")}>Data</li>
