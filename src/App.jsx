72376f0b63631ec139a26652845a3f5cd83890f9
javier garcia alvarez <javigarcial87@gmail.com>
2026-03-10 19:15:19 +0100

    instalacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,13 @@
-import './App.css'
 import {useState} from "react"
+import React from "react"
+import './App.css'
 
 function App() {
  
-    const [name, setName] = useState("Sofía")
-    const {newName,setNewName} = useState("")
 
-    const changeName = (e)=> {
-        e.preventDefault()
-
-        if (newName.trim()!=="") {
-            setName(newName)
-            setNewName("")
-        }
-    }
 
     return (
-        <div>
-            <h2>Nombre del profesor:{name}</h2>
-
-            <form onSubmit={changeName} >
-                <input 
-                type="text"
-                value ={newName}
-                onChange={(e) => setName(e.target.value)}
-                placeholder='añade un nombre' />
-
-                <button type="submit">Añadir</button>
-            </form>
-
-            <ul>
-                <li onClick={()=> setName("Data")}>Data</li>
-                <li onClick={()=> setName("Reyes")}>Reyes</li>
-                <li onClick={()=> setName("Yolanda")}>Yolanda</li>
-            </ul>
-
-        </div>
+        <>usContext</>
     )
 
 }
